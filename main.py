d05b7a2e325058400ccf8aedb39134e84440d9bc
SuupraaMK4 <83295718+SuupraaMK4@users.noreply.github.com>
2021-09-05 12:05:31 +0000

    Initial commit

--- main.py
+++ main.py
@@ -43,6 +43,15 @@ async def on_ready():
   await bot.change_presence(activity=discord.Streaming(name='SuupraaMK4', url='https://www.twitch.tv/zemmett'))
   print(f"{Fore.YELLOW}Logged as {Fore.RED}{bot.user}")
 
+bot.remove_command("help")
+
+@bot.command()
+async def help(ctx):
+  embed = discord.Embed(title='Ayuda', description='`test` - Comando de prueba\n`purge` - Borra todos tus mensajes en una chat\n`hypesquad` - Cambia tu casa de hypesquad', color=0x1C1C1C)
+  embed.set_footer(text='Youtube')
+  embed.set_thumbnail(url='https://cdn.discordapp.com/attachments/883782125508259910/884046054612406282/575cac3769dd5f9002300eb4913a2830.gif')
+  await ctx.send(embed=embed)
+
 @bot.command()
 async def test(ctx):
   await ctx.message.delete()
@@ -50,6 +59,42 @@ async def test(ctx):
   embed.set_image(url='https://cdn.discordapp.com/attachments/541858594098774018/883176453875834900/3176e8e54324fb86064f22b6db71ffdc_1.gif')
   embed.set_footer(text='Prueba Youtube')
   await ctx.send(embed=embed)
+
+@bot.command()
+async def purge(message):
+  async for msg in message.channel.history(limit=10000):
+    if msg.author == bot.user:
+      try:
+        await msg.delete()
+      except:
+        pass
+
+@bot.command()
+async def hypesquad(ctx, house):
+  await ctx.message.delete()
+  request = requests.session()
+  headers = {
+    'Authorization': token,
+    'Content-Type': 'application/json'
+  }
+
+  global payload
+
+  if house == "bravery":
+    payload = {'house_id': 1}
+  elif house == "brilliance":
+    payload = {'house_id': 2}
+  elif house == "balance":
+    payload = {'house_id': 3}
+
+  try:
+    requests.post('https://discordapp.com/api/v6/hypesquad/online', headers=headers, json=payload)
+    print(f"{Fore.GREEN}Has cambiado a {house} correctamente!")
+  except:
+    print(f"{Fore.RED}Ha ocurrido un error al intentar cambiarte a {house}")
+
+
+  
 
 
 
